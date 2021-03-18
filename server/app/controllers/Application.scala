9bef83965faa4e21c7f0b1e15458ceb8bbd40b96
Matthew Barton <mbarton@trinity.edu>
2021-03-18 12:16:23 -0500

    3/18 in class - finished testing

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -35,4 +35,8 @@ class Application @Inject()(cc: ControllerComponents) extends AbstractController
     }.getOrElse(Ok("Error"))
 
   }
+
+  def logout() = Action {
+    Ok(views.html.index(SharedMessages.itWorks))
+  }
 }
--- server/test/UsernameSpec.scala
+++ server/test/UsernameSpec.scala
@@ -1,10 +1,11 @@
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 
-class UsernameSpec extends PlaySpec with GuiceOneServerPerSuite with OneBrowserPerSuite with HtmlUnitFactory{
+class UsernameSpec extends PlaySpec with GuiceOneServerPerSuite with OneBrowserPerSuite with HtmlUnitFactory {
     "UsernameSpec" must {
         "login and enter color" in {
             go to s"http://localhost:$port"
+            pageTitle mustBe "Play with Scala.js"
             find(cssSelector("h2")).isEmpty mustBe false
             find(cssSelector("h2")).foreach(e => e.text mustBe "Play and Scala.js share a same message")
 
@@ -13,6 +14,11 @@ class UsernameSpec extends PlaySpec with GuiceOneServerPerSuite with OneBrowserP
             click on "color"
             textField("color").value = "Yellow"
             submit()
+            eventually {
+                pageTitle mustBe "Name and Color"
+                find(cssSelector("h2")).isEmpty mustBe false
+                find(cssSelector("h2")).foreach(e => e.text mustBe "Name entered: Thomas Lauerman")
+            }
         }
     }
 }
